1a1550942e90be219b97826afc6ec23b02446794
EliasKng <elias-koenig@web.de>
2021-12-12 12:42:52 +0100

    Add scss to Project

--- 04_Implementation/frontend/src/App.tsx
+++ 04_Implementation/frontend/src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import {Visualization} from "./Components/Visualization";
+import {VisualizationSettings} from "./Components/VisualizationSettings";
 
 function App() {
   return <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo"/>
-      <p id='test'>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <body>
+      <Visualization/>
+      <VisualizationSettings/>
+    </body>
   </div>;
 }
 
--- 04_Implementation/frontend/src/index.tsx
+++ 04_Implementation/frontend/src/index.tsx
@@ -3,22 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-const a =   <React.StrictMode>
-                <App />
-            </React.StrictMode>;
-ReactDOM.render(
-  a,
-  document.getElementById('root')
-);
 
-function Welcome(props: any) {
-    return <h1>Hello, {props.name}</h1>;
-}
-
-const element = <Welcome name="ABC" />;
 ReactDOM.render(
-    element,
-    document.getElementById('test'),
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
