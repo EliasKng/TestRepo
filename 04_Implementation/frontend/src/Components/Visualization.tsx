f998a02e4c340fad9330cbe0b152c7ce13e8d556
EliasKng <elias-koenig@web.de>
2021-12-12 12:43:00 +0100

    Add basic Components

---
+++ 04_Implementation/frontend/src/Components/Visualization.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export class Visualization extends React.Component {
+    render() {
+        return <div className='visualization'>
+            <h2>Visualization</h2>
+            {/*<img src='https://i.ibb.co/DGnv0zZ/image.png' alt='visImage' className='vis-image'/>*/}
+        </div>;
+    }
+}
---
+++ 04_Implementation/frontend/src/Components/VisualizationSettings.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class VisualizationSettings extends React.Component {
+    render() {
+        return <div className='visualization-settings'>
+            <h2>Visualization settings</h2>
+        </div>;
+    }
+}
