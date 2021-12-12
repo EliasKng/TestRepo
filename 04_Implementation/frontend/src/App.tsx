fc47a658d07f726233e3c0dd5bfe3214579ea681
EliasKng <elias-koenig@web.de>
2021-12-12 13:01:04 +0100

    Add basic Layout

--- 04_Implementation/frontend/src/App.tsx
+++ 04_Implementation/frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import {Visualization} from "./Components/Visualization";
 import {VisualizationSettings} from "./Components/VisualizationSettings";
+import {Summary} from "./Components/Summary";
 
 function App() {
   return <div className="App">
     <body>
       <Visualization/>
       <VisualizationSettings/>
+      <Summary/>
     </body>
   </div>;
 }
